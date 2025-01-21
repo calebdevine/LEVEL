c00aec0ffd8229f54c8ee2b07028838d721f36c2
Caleb <c.devine@studenti.unipi.it>
2025-01-21 10:59:57 +0100

    style changes

--- level.sql
+++ level.sql
@@ -6,6 +6,7 @@ USE `level`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`(
     `userId`              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `role`                VARCHAR(255) NOT NULL DEFAULT 'costumer',
     `name`                VARCHAR(100) NOT NULL,
     `surname`             VARCHAR(100) NOT NULL,
     `idCode`              VARCHAR(100) NOT NULL,
@@ -18,10 +19,10 @@ CREATE TABLE `user`(
     `med_certifcate`      BOOLEAN DEFAULT 0,
     `certifcate_expDate`  DATE    
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `user` (`userId`, `name`, `surname`, `idCode`, `email`, `password`, `phone`, `state`) VALUES 
-(1, 'CALEB', 'DEVINE', 'dvncbs99e21f704k', 'caleb.devine.cd@gmail.com', '$2y$10$QhHziHte4fx.rok.LXzXVeFC2IHj3s4.SLh2aXpgYVOXEuBRzWI/e', '3345393392', '1'),
-(NULL, 'Admin', '', '', 'admin', '$2y$10$oqDeZxymihgdEddvks7KEus32eFwaTmSy3WfVzLfNJx0og4lbjqza', '', '-1'),
-(NULL, 'Mario', 'Rossi', 'MRORSS65A19G842R', 'mario_rossi@example.com', '$2y$10$z4qABUrStoFiwAuCn3Jw6O7VSJERbSTETpmPMQqLFThcwHhucDkPa', '3289423583', '1');
+INSERT INTO `user` (`userId`, `role`,`name`, `surname`, `idCode`, `email`, `password`, `phone`, `state`) VALUES 
+(1,'', 'CALEB', 'DEVINE', 'dvncbs99e21f704k', 'caleb.devine.cd@gmail.com', '$2y$10$QhHziHte4fx.rok.LXzXVeFC2IHj3s4.SLh2aXpgYVOXEuBRzWI/e', '3345393392', '1'),
+(0, 'admin','Amministratore', '', '', 'levelasd@gmail.com', '$2y$10$oqDeZxymihgdEddvks7KEus32eFwaTmSy3WfVzLfNJx0og4lbjqza', '', '-1'),
+(2, '','Mario', 'Rossi', 'MRORSS65A19G842R', 'mario_rossi@example.com', '$2y$10$z4qABUrStoFiwAuCn3Jw6O7VSJERbSTETpmPMQqLFThcwHhucDkPa', '3289423583', '1');
 
 DROP TABLE IF EXISTS `course`;
 CREATE TABLE course (
@@ -34,16 +35,16 @@ CREATE TABLE course (
     `max_places`    INT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 INSERT INTO `course` (`courseId`, `course_name`, `day`, `start`, `end`, `trainer`, `max_places`) VALUES
-(0,"TABATA", "Lunedì", "12:00","12:45","Ludovica", 15),
-(1,"TABATA", "Venerdì", "12:00","12:45","Ludovica", 15),
-(2,"TOTALBODY", "Lunedì", "18:00","19:00","Ludovica", 15),
-(3,"TOTALBODY", "Giovedì", "19:00","20:00","Ludovica", 15),
-(4,"TOTALBODY", "Venerdì", "17:00","18:00","Ludovica", 15),
-(5,"PILATES", "Giovedì", "17:00","18:00", "Alessia", 15),
-(6,"PILATES", "Venerdì", "19:00","20:00", "Alessia", 15),
-(7,"GAG", "Giovedì", "18:00","19:00","Ludovica", 15),
-(8,"PILATES", "Martedì", "19:00","20:00", "Alessia", 15),
-(9,"HIIT", "Lunedì", "19:00","20:00","Ludovica", 15);
+(1,"TABATA", "Lunedì", "12:00","12:45","Ludovica", 15),
+(2,"TABATA", "Venerdì", "12:00","12:45","Ludovica", 15),
+(3,"TOTALBODY", "Lunedì", "18:00","19:00","Ludovica", 15),
+(4,"TOTALBODY", "Giovedì", "19:00","20:00","Ludovica", 15),
+(5,"TOTALBODY", "Venerdì", "17:00","18:00","Ludovica", 15),
+(6,"PILATES", "Giovedì", "17:00","18:00", "Alessia", 15),
+(7,"PILATES", "Venerdì", "19:00","20:00", "Alessia", 15),
+(8,"GAG", "Giovedì", "18:00","19:00","Ludovica", 15),
+(9,"PILATES", "Martedì", "19:00","20:00", "Alessia", 15),
+(10,"HIIT", "Lunedì", "19:00","20:00","Ludovica", 15);
 
 DROP TABLE IF EXISTS `reservations`;
 CREATE TABLE reservations (
--- php/layout/footer.php
+++ php/layout/footer.php
@@ -1,18 +1,18 @@
 
     <div class="info" id= "info">
         <h2>Contatti</h2>
-        <div>
+        <div class="contacts">
             <section  class="telefono">
                 <p><a href="tel:+393347011825"><img src="../../img/logocell.png" alt="logo-cell"></a></p>
-                <p>Telefono: <a class ="no-underline" href="tel:+393347011825"> +39 3347011825</a></p>
+                <p class="social-text">Telefono: <a class ="no-underline" href="tel:+393347011825"> +39 3347011825</a></p>
             </section>
             <section class="email">
                 <p><a href="mailto:levelasd@gmail.com"><img src="../../img/logomail.png" alt="logo-mail"></a></p>
-                <p>Email: <a class ="no-underline" href="mailto:levelasd@gmail.com">levelasd@gmail.com </a></p>
+                <p class="social-text">Email: <a class ="no-underline" href="mailto:levelasd@gmail.com">levelasd@gmail.com </a></p>
             </section>
             <section class="social">
                 <p><a href="https://www.instagram.com/levelpisaasd/"><img src="../../img/logoinsta.png" alt="logo-instagram"></a></p>
-                <p>Seguici su <a  href="https://www.instagram.com/levelpisaasd/">Instagram</a> !</p>
+                <p class="social-text">Seguici su <a  href="https://www.instagram.com/levelpisaasd/">Instagram</a> !</p>
             </section>
         </div>
         
